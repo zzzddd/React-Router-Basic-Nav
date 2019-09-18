e2010a0935f7705b6a9b75056ef0b08202a64a60
Chris Dalao <chrisdalao@gmail.com>
2019-09-17 23:17:35 -0700

    Revert "part 1 of react router.."

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
-import { Route } from "react-router-dom";
 
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/" component={Home} />
-    <Route path="/Contact" component={Contact} />
-    <Route path="/About" component={About} />
   </div>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from "react-router-dom";
 
 const Navigation = () => {
   return (
@@ -7,16 +6,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          {/* <a href="">Home</a> */}
-          <Link to="/">Home</Link>
+          <a href="">Home</a>
         </div>
         <div>
-          {/* <a href="">About</a> */}
-          <Link to="/about">About</Link>
+          <a href="">About</a>
         </div>
         <div>
-          {/* <a href="">Contact</a> */}
-          <Link to="/Contact">Contact</Link>
+          <a href="">Contact</a>
         </div>
       </div>
     </div>
